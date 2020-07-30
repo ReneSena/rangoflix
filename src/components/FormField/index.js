fb4e909b1b31223c743dee69d2366166b94f2e25
renedocket <rene.sena3005@gmail.com>
2020-07-30 19:11:36 -0300

    Instalação do eslint + proptypes + json-server e config dessas dependências.

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,34 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import { FieldWrapper, Field, FieldArea, Label } from './styled';
+import {
+  FieldWrapper, Field, FieldArea, Label,
+} from './styled';
 
-const FormField = ({ type, name, value, onChange, label }) => {
-    return(
-        <FieldWrapper>
-            {type === 'textarea' ? 
-                <FieldArea 
-                    type={type} 
-                    name={name}
-                    value={value}
-                    onChange={onChange}
-                    required
-                    rows="5"
-                    cols="30"
-                /> :  
-                <Field
-                    type={type} 
-                    name={name}
-                    value={value}
-                    onChange={onChange}
-                    required
-                />
-            }
-        
-            <Label className="description-label">
-                {label}
-            </Label>
-        </FieldWrapper>
-    );
-}
+const FormField = ({
+  type, name, value, onChange, label,
+}) => (
+  <FieldWrapper>
+    {type === 'textarea'
+      ? (
+        <FieldArea
+          type={type}
+          name={name}
+          value={value}
+          onChange={onChange}
+          required
+          rows="5"
+          cols="30"
+        />
+      )
+      : (
+        <Field
+          type={type}
+          name={name}
+          value={value}
+          onChange={onChange}
+          required
+        />
+      )}
+
+    <Label className="description-label">
+      {label}
+    </Label>
+  </FieldWrapper>
+);
+
+FormField.defaultProps = {
+  type: 'text',
+  value: '',
+  onChange: () => {},
+};
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
 
 export default FormField;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,99 +1,127 @@
-import React, { useState }  from 'react';
+import React, { useState, useEffect } from 'react';
 import Template from '../../../components/Template';
 import FormField from '../../../components/FormField';
 
-import { Title, Form, ButtonSave, Table } from './styled';
+import {
+  Title, Form, ButtonSave, Table,
+} from './styled';
 
 const CadastroCategoria = () => {
-    const [listCategorys, setListCategorys] = useState([]); //Guarda os valores da lista de categorias
+  const initialValues = {
+    name: '',
+    description: '',
+    color: '#20bf6b',
+  };
 
-    const initialValues = {
-        name: '',
-        description: '',
-        color: '#20bf6b', 
-    }
+  // Guarda os valores da lista de categorias
+  const [listCategorys, setListCategorys] = useState([]);
+  const [values, setValues] = useState(initialValues);
 
-    const [values, setValues] = useState(initialValues);
+  const setValue = (chave, valor) => {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  };
 
-    const setValue = (chave, valor)=> {
-        setValues({
-            ...values,
-            [chave]: valor,
-        })
-    } 
+  const handleChange = (event) => {
+    setValue(
+      event.target.getAttribute('name'), // nome campo
+      event.target.value, // valor digitado no input
+    );
+  };
 
-    const handleChange = (event) => {
+  useEffect(() => {
+    // O que a gente quer que aconteça
+    const urlValue = 'http://localhost:8080/categorias';
 
-        setValue(
-            event.target.getAttribute('name'), //nome campo
-            event.target.value //valor digitado no input
-        );
-    }
-    
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    fetch(urlValue)
+      .then(async (res) => {
+        const responseReq = await res.json();
+        // console.log(listCategorys)
+        setListCategorys([
+          ...responseReq,
+        ]);
+      });
+  }, []);
+  /* quando a quer que aconteça */
 
-        setListCategorys([...listCategorys, values]);
-        setValues(initialValues)
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    return(
-        <Template styled={{ textAlign:'center'}}>
-            <Title>Nova Categoria</Title>
-            
-            <Form onSubmit={handleSubmit}>
-                <FormField 
-                   label="Nome"
-                   type="text" 
-                   name="name"
-                   value={values.name}
-                   onChange={handleChange}
-                /> 
+    setListCategorys([...listCategorys, values]);
+    setValues(initialValues);
+  };
 
-                <FormField 
-                   label="Descrição"
-                   type="textarea" 
-                   name="description"
-                   value={values.description}
-                   onChange={handleChange}
-                /> 
-                
-                <FormField 
-                   label="Color"
-                   type="color" 
-                   name="color"
-                   value={values.color}
-                   onChange={handleChange}
-                /> 
+  return (
+    <Template styled={{ textAlign: 'center' }}>
+      <Title>Nova Categoria</Title>
 
-                <ButtonSave type="submit">Cadastrar</ButtonSave>
-            </Form>
-  
-            {listCategorys.length > 0 && 
+      <Form onSubmit={handleSubmit}>
+        <FormField
+          label="Nome"
+          type="text"
+          name="name"
+          value={values.name}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="description"
+          value={values.description}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Color"
+          type="color"
+          name="color"
+          value={values.color}
+          onChange={handleChange}
+        />
+
+        <ButtonSave type="submit">Cadastrar</ButtonSave>
+      </Form>
+
+      {/* <ul>
+        {listCategorys.map((category) => {
+          return (
+            <li key={category.nome}>{category.cor}</li>
+          );
+        })}
+      </ul> */}
+
+      {listCategorys.length === 0 && (
+        <div>Loading...</div>
+      )}
+
+      {listCategorys.length > 0
+                && (
                 <Table>
-                    <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>Descrição</th>
-                            <th>Cor</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {listCategorys.map((category, index) => {
-                            return(
-                                <tr key={index}>
-                                    <td>{category.name}</td>
-                                    <td>{category.description}</td>
-                                    <td>{category.color}</td>
-                                </tr>     
-                            )
-                        })}
-                    </tbody>
+                  <thead>
+                    <tr>
+                      <th>Nome</th>
+                      <th>Descrição</th>
+                      <th>Cor</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {listCategorys.map((category, index) => (
+                      // eslint-disable-next-line react/no-array-index-key
+                      <tr key={index}>
+                        <td>{category.name}</td>
+                        <td>{category.description}</td>
+                        <td>{category.color}</td>
+                      </tr>
+                    ))}
+                  </tbody>
                 </Table>
-            }
-     
-        </Template>
-    )
+                )}
+
+    </Template>
+  );
 };
 
 export default CadastroCategoria;
