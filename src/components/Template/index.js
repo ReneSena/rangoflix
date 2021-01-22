66c8b08ecb76f18d6f874b77be8dbf1d2ea02c49
ReneSena <rene.sena3005@gmail.com>
2021-01-21 21:38:14 -0300

    Validação para aparecer o footer apenas na home.

--- src/components/Template/index.js
+++ src/components/Template/index.js
@@ -1,20 +1,19 @@
-import React from 'react';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Menu from "../Menu";
+import Footer from "../Footer";
 
-import Menu from '../Menu';
-import Footer from '../Footer';
-
-import { Main } from './styled';
+import { Main } from "./styled";
 
 const Template = ({ children }) => {
-    return(
-        <>
-            <Menu />
-                <Main>
-                    {children}
-                </Main>
-            <Footer />
-        </>
-    )
-}
+	const { pathname } = useLocation();
+	return (
+		<>
+			<Menu />
+			<Main>{children}</Main>
+			{!pathname.includes("/cadastro") && <Footer />}
+		</>
+	);
+};
 
 export default Template;
--- src/components/Template/styled.js
+++ src/components/Template/styled.js
@@ -1,10 +1,10 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Main = styled.main`
-    background-color: var(--black);
-    color: var(--white);
-    flex: 1;
-    /* padding-top: 50px;
+	background-color: #f3f3f3;
+	color: var(--white);
+	flex: 1;
+	/* padding-top: 50px;
     padding-left: 5%;
     padding-right: 5%; */
-`
+`;
